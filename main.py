1d75259ecde0bf0f4c99220079e98759a6c38ded
dinoduck22 <124132148+dinoduck22@users.noreply.github.com>
2023-11-26 20:35:54 +0900

    added AI chat bot in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from PIL import Image
 import base64
 from st_audiorec import st_audiorec
+from openai import OpenAI
 
 # 레이아웃
 st.set_page_config(layout="wide")
@@ -10,15 +10,27 @@ if 'count' not in st.session_state:  # 버튼 클릭 횟수
     st.session_state.count = 0
 if 'caption' not in st.session_state:  # 버튼 내용
     st.session_state['caption'] = 'caption place'
-if 'message' not in st.session_state:  # chat history
-    st.session_state.message = []
+
+# Set OpenAI API key from Streamlit secrets
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+# AI response
+question = "Hello, this is AI mock Interview."
+
+# Initialize chat history
+if 'messages' not in st.session_state:
+    st.session_state.messages = [{"role": "user", "content": ""}]
 
 
-def caption(button):
-    st.session_state['caption'] = button
+def caption(string):
+    st.session_state['caption'] = string
     st.session_state.count += 1
     if st.session_state.count == 2:
-        st.session_state['caption'] = 'caption'  # 버튼 클릭 시 표기
+        st.session_state['caption'] = 'caption place'  # 버튼 클릭 시 표기
         st.session_state.count = 0
 
 
@@ -32,20 +44,27 @@ def record():
 # 사이드 바
 def sidebar():
     st.sidebar.title("Chat Log")
-    for message in st.session_state.message:  # Display chat history
-        with st.sidebar.chat_message(message["role"]):
-            st.markdown(message["content"])
-    # Chat Input
+    # Display chat history
+    for messages in st.session_state.messages:
+        with st.sidebar.chat_message(messages["role"]):
+            st.markdown(messages["content"])
+    # Chat Input & AI question
     if prompt := st.chat_input("Type in your Answer"):
+        st.session_state.messages.append({"role": "user", "content": prompt})  # Add to chat history
         with st.sidebar.chat_message("user"):
             st.markdown(prompt)  # Display user message
-        st.session_state.message.append({"role": "user", "content": prompt})  # Add to chat history
-    # AI response
-    question = "Hello, this is AI mock Interview."
-    with st.sidebar.chat_message("assistant"):
-        st.markdown(question)  # Display assistant response
-    st.session_state.message.append({"role": "assistant", "content": question})  # Add to chat history
-
+        with st.sidebar.chat_message("assistant"):  # Display assistant response in chat message container
+            message_placeholder = st.empty()
+            full_response = ""
+        for response in client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+                stream=True,
+        ):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 def main():
     # 사이드 바
@@ -58,20 +77,29 @@ def main():
     contents = file_.read()
     data_url = base64.b64encode(contents).decode("utf-8")
     file_.close()
-    st.markdown(
-        f'<img src="data:image/gif;base64,{data_url}" alt="job-interview" width="100%">',
-        unsafe_allow_html=True,
-    )
-    # 버튼
-    col1, col2 = st.columns([1, 1])  # 비율 col1:col2
-    with col1:
-        st.text('')  # 줄바꿈
-        st.button(label="caption", on_click=caption('clicked'))
+    # 화면
+    col1, col2, col3 = st.columns([0.2, 1, 0.2])  # 비율 col1:col2:col3
     with col2:
+        st.markdown(
+            f'<img src="data:image/gif;base64,{data_url}" alt="job-interview" width="100%">',
+            unsafe_allow_html=True,
+        )
+        col_l, col_m, col_r = st.columns([0.1, 1, 0.1])  # 비율 col_l:col_m:col_r
+        with col_m:
+            # Recording
+            record()
+    co_button, co_caption = st.columns([0.4, 1])  # 비율 co_button:co_caption
+    # 사용자 화면
+    st.camera_input(label="User screen", key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False,
+                    label_visibility="visible")
+
+    # 버튼
+    with co_button:
+        st.text('')  # 줄바꿈
+        st.button(label="caption", on_click=caption(question))
+    with co_caption:
         st.text('')  # 줄바꿈
         st.text(st.session_state['caption'])
-    # Recording
-    record()
 
 
 if __name__ == '__main__':
